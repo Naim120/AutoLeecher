634c3933fd8c8bb0e64c1f1e87f1556eaf293781
sk naim <sknaimuddin234@gmail.com>
2022-01-18 11:21:51 +0530

    Update conf.py

--- apdbot/conf.py
+++ apdbot/conf.py
@@ -2,8 +2,8 @@ from apdbot.sample_con import Config
 #Fill your all data here Do not Edit other Files
 
 class Config(Config):
-    BOT_TOKEN= ""
-    APP_ID = 
-    API_HASH = ""
-    OWNER_ID = ""
-    AUTH = [556315181, -100088888, -10002252254]
+    BOT_TOKEN= "5047078690:AAGi11Cn0boPwCqwMy0-65qhC7cQGNLQcbI"
+    APP_ID = 1567102
+    API_HASH = "07e9ab27a1d8d479471f81e7848731ad"
+    OWNER_ID = "1185346119"
+    AUTH = [-1001403236771]
